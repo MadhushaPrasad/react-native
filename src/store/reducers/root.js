8e0091ab50ea5b57bdf7332e2f926778db1ca4cc
Madhusha Prasad <madushaprasad21@gmail.com>
2019-11-07 16:59:39 +0530

    current update testing react-native project

---
+++ src/store/reducers/root.js
@@ -0,0 +1,17 @@
+
+
+const initialState={
+    places:[],
+    selectPlace:null
+    
+};
+const reducerName = (state = initialState, action) => {
+    switch (action.type) {
+        case 'ACTION_TYPE':
+            return 
+        default:
+            return state
+    }
+}
+
+export default root;
